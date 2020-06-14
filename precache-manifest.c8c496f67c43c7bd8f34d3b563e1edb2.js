45e22527b4c44a7b82dcb7850c6ab5956d2dab18
lucasew <lucas59356@gmail.com>
2020-06-14 13:30:05 +0000

    Updates

---
+++ precache-manifest.c8c496f67c43c7bd8f34d3b563e1edb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2c09b085be301e93b9c8e4a12a8f47",
+    "url": "/react_auxilio_emergencial/index.html"
+  },
+  {
+    "revision": "f67324b25c0c39fb24b0",
+    "url": "/react_auxilio_emergencial/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ea814dcf86dbb4415b93",
+    "url": "/react_auxilio_emergencial/static/css/main.54b6b645.chunk.css"
+  },
+  {
+    "revision": "f67324b25c0c39fb24b0",
+    "url": "/react_auxilio_emergencial/static/js/2.5918dc79.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_auxilio_emergencial/static/js/2.5918dc79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea814dcf86dbb4415b93",
+    "url": "/react_auxilio_emergencial/static/js/main.25ff54b2.chunk.js"
+  },
+  {
+    "revision": "d8a4dc213f9a1cd842dc",
+    "url": "/react_auxilio_emergencial/static/js/runtime-main.a0c32f16.js"
+  }
+]);
