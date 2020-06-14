cf96c992616c93c770fb0d5340b5468ad0835a5a
lucasew <lucas59356@gmail.com>
2020-06-14 13:41:07 +0000

    Tentando migraçao para o hash router
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Route, BrowserRouter, Switch} from 'react-router-dom'
+import {Route, HashRouter, Switch} from 'react-router-dom'
 
 import Home from './pages/Home'
 import ListBeneficiarios from './pages/ListBeneficiarios'
@@ -9,14 +9,14 @@ import Header from './components/Header'
 
 const Routes = () => {
     return (
-        <BrowserRouter basename="/react_auxilio_emergencial">
+        <HashRouter basename="/">
             <Header/>
             <Switch>
                 <Route component={Home} path="/" exact />
                 <Route component={ListBeneficiarios} path="/show-cidade/:cidadeId/:periodo" />
                 <Route component={NotFound} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
